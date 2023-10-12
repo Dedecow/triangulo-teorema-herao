9fce37d5303692fe21f3ea540f2f470ae8d9860c
Andre <andrehbahia@gmail.com>
2023-10-12 10:50:28 -0300

    calculo-triangulo-inclui-logica-dowhile-scScanner

--- src/com/udemy/programacaoorientadaaobjetos/TesteTriangulo.java
+++ src/com/udemy/programacaoorientadaaobjetos/TesteTriangulo.java
@@ -7,13 +7,20 @@ public class TesteTriangulo {
 
         Triangulo[] triangulos = new Triangulo[3];
         Scanner scn = new Scanner(System.in);
+        int i = 0;
 
-        triangulos[0] = new Triangulo(3.0f, 4.0f, 5.0f);
-        triangulos[1] = new Triangulo(2.0f, 2.0f, 2.0f);
-        triangulos[2] = new Triangulo(1.0f, 2.0f, 3.0f);
-
-
-        for(int i = 0; i<triangulos.length; i++) {
+        do {
+            System.out.println(" Digite o valor do primeiro lado");
+            float lado1 = Float.parseFloat(scn.nextLine());
+            System.out.println(" Digite o valor do segundo lado");
+            float lado2 = Float.parseFloat(scn.nextLine());
+            System.out.println(" Digite o valor do terceiro lado");
+            float lado3 = Float.parseFloat(scn.nextLine());
+            triangulos[i] = new Triangulo(lado1, lado2, lado3);
+            i++;
+        } while (i < 3);
+        i=0;
+        do {
             if(triangulos[i] != null) {
                 System.out.println("Triângulo " + i + ":");
                 System.out.println("Lado 1: " + triangulos[i].getLado1());
@@ -23,8 +30,10 @@ public class TesteTriangulo {
                 System.out.println("Área: " + triangulos[i].area());
                 System.out.println("Válido: " + triangulos[i].valido());
                 System.out.println();
+                i++;
             }
-        }
+        }while (i<triangulos.length);
+
         System.out.println("Quantidade de instâncias de Triangulo: " + Triangulo.getQuantidade());
     }
 }
